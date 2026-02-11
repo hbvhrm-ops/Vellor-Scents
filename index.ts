b8cd592f8654ebe076c501688a1257f567ab4acd
Hamza Haidar <hbvhrm@gmail.com>
2026-02-12 00:08:04 +0500

    Delete index.ts

--- index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ProductModal } from './ProductModal';
-export { default as CartDrawer } from './CartDrawer';
-export { default as CheckoutModal } from './CheckoutModal';
-export { default as AdminDashboard } from './AdminDashboard';
